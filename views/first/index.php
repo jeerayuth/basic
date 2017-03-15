6f5e1e989db76aab21fe0c257b52b5ee289da64e
user <user@ADMIN-CENTER>
2017-03-15 16:33:36 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- views/first/index.php
+++ views/first/index.php
@@ -1,10 +1,19 @@
 <?php
 $this->title = 'First Menu';
 $this->params['breadcrumbs'][] = $this->title;
-?>
-<h1>/first/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+// copy มาจากตัวอย่าง page4
+// เพื่อทำระบบ Menu
+$link1 = Yii::$app->urlManager->createUrl([
+    'first/page1',
+]);
+
+?>
+
+
+
+<h1>/first/index</h1>
+<!-- ส่วนแสดงค่าเมนู -->
+<a href="<?= $link1; ?>">
+    1. การแสดงค่าจาก view
+</a>
