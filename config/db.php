352586b2c4c17f18725d472ee3f2f5943e148237
user <user@ADMIN-CENTER>
2017-03-16 08:34:22 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=192.168.1.225:3306;dbname=basic',
+    'username' => 'hospital',
+    'password' => 'lamae',
     'charset' => 'utf8',
 ];
